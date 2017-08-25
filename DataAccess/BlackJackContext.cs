397d7217c4fcf18eb4ef8a515b9f73fec7a34e42
zencifil <savascilve@gmail.com>
2017-08-25 11:55:37 +0100

    db context created

---
+++ DataAccess/BlackJackContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace blackjack.DataAccess {
+
+	public partial class BlackJackContext : DbContext {
+		
+		public DbSet<PlayLog> PlayLog { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(Constants.SqlConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<PlayLog>()
+                        .Property(e => e.SessionID)
+                        .IsUnicode(false);
+        }
+
+	}
+}
---
+++ DataAccess/Constants.cs
@@ -0,0 +1,6 @@
+﻿using System;
+namespace blackjack.DataAccess {
+    public static class Constants {
+        public const string SqlConnectionString = "";
+    }
+}
