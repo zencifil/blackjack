80404998b0885ff49348ee333f454df95a8cc6b1
zencifil <savascilve@gmail.com>
2017-08-25 09:59:31 +0100

    project created

---
+++ DataAccess/PlayLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace blackjack.DataAccess {
+	[Table("PlayLog")]
+	public partial class PlayLog {
+		public int ID { get; set; }
+
+		[StringLength(50)]
+		public string SessionID { get; set; }
+
+		public int? Cash { get; set; }
+
+		public bool? Win { get; set; }
+
+		public int? PutCache { get; set; }
+
+		public DateTime? CreatedDate { get; set; }
+	}
+}
